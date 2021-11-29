b93c03039b2888d98422d15727d63ca90ad11804
muskanrustagi24 <muskanrustagi24@gmail.com>
2021-11-29 16:48:45 +0530

    AddedLocalStorage

--- react-todo-app/src/components/Todo.js
+++ react-todo-app/src/components/Todo.js
@@ -24,7 +24,7 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
       key={index}
     >
       <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
+        {todo.text} 
       </div>
       <div className="icons">
         <RiCloseCircleLine
